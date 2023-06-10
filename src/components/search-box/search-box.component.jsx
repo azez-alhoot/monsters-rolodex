a4e4875691d76d7b69a7c0731d32f7f382dddfab
Azez Alhoot <azezalhoot82@yahoo.com>
2023-06-10 20:07:56 +0300

    app fixes and congigure docker

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,16 @@
+import "./search-box.styles.css"
+
+const SearchBox = (props) =>  {
+    const { className, placeholder, onChangeHandler } = props;
+
+    return (
+      <input
+        className={className}
+        type="search"
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+      />
+    );
+}
+
+export default SearchBox;
